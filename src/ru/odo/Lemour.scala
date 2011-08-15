600bf4026ecb5951bdcc69ca9adf7263f96420aa
Odomontois <odomontois@gmail.com>
2011-08-15 09:36:40 +0400

    ff

---
+++ src/ru/odo/Lemour.scala
@@ -0,0 +1,45 @@
+package ru.odo
+import javafx.application.Application
+import javafx.stage.Stage
+import javafx.scene.layout.BorderPane
+import javafx.scene.shape.Rectangle
+import javafx.scene.paint.Color
+import javafx.scene.Scene
+import javafx.scene.layout.HBox
+import javafx.scene.control.Button
+import javafx.geometry.Insets
+import javafx.geometry.Pos
+
+object Lemour {
+
+  def main(args: Array[String]): Unit = Application.launch(classOf[LemourAppl],args)
+
+}
+
+class LemourAppl extends Application{
+  override def start(mainStage : Stage) = {
+	mainStage.setScene(scene)
+    mainStage.setVisible(true)
+  }
+  lazy val scene = new Scene(borderPane,200,200,Color.BLACK)
+  lazy val borderPane = {
+    val borderPane = new BorderPane
+    borderPane.setTop(new Rectangle(200,50, Color.DARKCYAN))
+    borderPane.setBottom(bottomPanel)
+    borderPane.setLeft(new Rectangle(50,100,Color.DARKTURQUOISE))
+    borderPane.setRight(new Rectangle(50,100,Color.DARKTURQUOISE))
+    borderPane.setCenter(new Rectangle(100,100,Color.MEDIUMAQUAMARINE))
+    borderPane
+  }
+ lazy val bottomPanel ={
+   val bottomPanel = new HBox
+   bottomPanel.setPadding(new Insets(15,12,15,12))
+   bottomPanel.setSpacing(10)
+   bottomPanel.setStyle("-fx-background-color: #336699")
+   bottomPanel.getChildren.addAll(buttonCurrent, buttonProjected)
+   bottomPanel.setAlignment(Pos.CENTER)
+   bottomPanel
+ }
+ lazy val buttonCurrent = new Button("Current")
+ lazy val buttonProjected = new Button("Projected")
+}
---
+++ src/ru/odo/NukeJane.scala
@@ -0,0 +1,87 @@
+package ru.odo
+import javafx.application.Application
+import javafx.stage.Stage
+import javafx.scene.Group
+import javafx.scene.shape._
+import javafx.scene.paint._
+import javafx.scene.effect._
+import javafx.animation._
+import javafx.scene.Scene
+import javafx.util.Duration
+import java.lang.Math.random
+import javafx.beans.value.WritableValue
+import javafx.beans.property.DoubleProperty
+import scala.collection.JavaConversions._
+import NukeJaneAppl._;
+
+
+object NukeJane {
+
+  def main(args: Array[String]): Unit = Application.launch(classOf[NukeJaneAppl], args)
+
+}
+
+class NukeJaneAppl extends Application {
+  override def start(primaryStage: Stage) = {
+    primaryStage.setScene(scene)
+    root.getChildren.add(blendMode);
+    timeline.play
+    primaryStage.setVisible(true)
+  }
+  private lazy val root = new Group
+  private lazy val scene = new Scene(root, 800, 600, Color.BLACK)
+  private lazy val circles = {
+    val circles = new Group
+    for (i <- 0 until 30) {
+      val circle = new Circle(150, Color.web("white", 0.05))
+      circle.setStrokeType(StrokeType.OUTSIDE)
+      circle.setStroke(Color.web("white", 0.16))
+      circle.setStrokeWidth(4)
+      circles.getChildren.add(circle)
+    }
+    circles.setEffect(new BoxBlur(10, 10, 3))
+    circles
+  }
+
+  private lazy val colors = {
+    val colors = new Rectangle(scene.getWidth, scene.getHeight,
+      new LinearGradient(0., 1., 1., 0., true, CycleMethod.NO_CYCLE,
+        new Stop(0, Color.web("#f8bd55")),
+        new Stop(0.14, Color.web("#c0fe56")),
+        new Stop(0.28, Color.web("#5dfbc1")),
+        new Stop(0.43, Color.web("#64c2f8")),
+        new Stop(0.57, Color.web("#be4af7")),
+        new Stop(0.71, Color.web("#ed5fc2")),
+        new Stop(0.85, Color.web("#ef504c")),
+        new Stop(1, Color.web("#f2660f"))))
+    colors
+  }
+  private lazy val timeline = {
+    val timeline = new Timeline
+    for (circle <- circles.getChildren) {
+      timeline.getKeyFrames.addAll(
+        new KeyFrame(Duration.ZERO,
+          new KeyValue(circle.translateXProperty, random * 800),
+          new KeyValue(circle.translateYProperty, random * 600)),
+        new KeyFrame(new Duration(40000),
+          new KeyValue(circle.translateXProperty, random * 800),
+          new KeyValue(circle.translateYProperty, random * 600)))
+    }
+    timeline
+  }
+  private lazy val blendMode = {
+    val blendMode = new Group(
+      new Group(
+        new Rectangle(scene.getWidth, scene.getHeight, Color.BLACK),
+        circles),
+      colors)
+    colors.setBlendMode(BlendMode.OVERLAY)
+    blendMode
+  }
+}
+
+object NukeJaneAppl {
+  implicit def writableValueDown [T <: WritableValue[Number]] (property : T): WritableValue[Any] = property.asInstanceOf[WritableValue[Any]]
+}
+
+
